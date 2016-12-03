cd81b6f719be49c4b374c3c64255f7120ac3f23a
Jake Bladt <jake@jakebladt.com>
2016-12-02 21:44:30 -0500

    +2=2 test

--- test/index.js
+++ test/index.js
@@ -1,4 +1,9 @@
 var should = require('chai').should(),
   poker = require('../index');
 
+describe('#face', function() {
+  it('equates deuce and two', function() {
+    poker.faces.deuce.should.equal(poker.faces.two);
+  });
 
+});
